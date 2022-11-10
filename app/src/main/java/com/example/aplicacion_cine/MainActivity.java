15c4a4e5b8427ca943f773ee9afcb0865bbe6418
monivelandia <monivelandia22@gmail.com>
2022-11-10 12:06:36 -0500

    Avanceregistro

--- app/src/main/java/com/example/aplicacion_cine/MainActivity.java
+++ app/src/main/java/com/example/aplicacion_cine/MainActivity.java
@@ -4,16 +4,30 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+
+import com.google.android.material.textfield.TextInputEditText;
 
 public class MainActivity extends AppCompatActivity {
     TextView mTextViewRegister = findViewById(R.id.TextViewRegister);
+    TextInputEditText mTextInputEditTextEmail = findViewById(R.id.TextInputEditTextEmail);
+    TextInputEditText mTextInputEditTextPassword = findViewById(R.id.TextInputEditTextPassword);
+    Button mButtonlogin = findViewById(R.id.btnlogin);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        mButtonlogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                login();
+            }
+        });
+
         mTextViewRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -21,5 +35,13 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    private void login() {
+        String email = mTextInputEditTextEmail.getText().toString();
+        String password = mTextInputEditTextPassword.getText().toString();
+        Log.d("campo", "email" + email);
+        Log.d("campo", "password" + password);
+
     }
 }
--- app/src/main/java/com/example/aplicacion_cine/RegisterActivity.java
+++ app/src/main/java/com/example/aplicacion_cine/RegisterActivity.java
@@ -3,13 +3,24 @@ package com.example.aplicacion_cine;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class RegisterActivity extends AppCompatActivity {
-
+    CircleImageView mCircleImageViewBack;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        mCircleImageViewBack = findViewById(R.id.circleimageback);
+        mCircleImageViewBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
+
 }
